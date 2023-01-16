ee0d3f06b5e3e4ce7f8098e5c5f05535e34aa565
coumba <coumbacoulibaly09@gmail.com>
2023-01-16 15:05:20 +0000

    Update Product_Analysis.sql

--- Product Analysis/Product_Analysis.sql
+++ Product Analysis/Product_Analysis.sql
@@ -17,60 +17,157 @@ USE AdventureWorks2019;
 SELECT COUNT(*) AS TotalNumberProduct
 FROM Production.Product;
 
-SELECT *
-FROM Production.Product;
-
 --2. How many product do we have by category? by sub-category?
 -- By Subcategory
-SELECT ps.[Name] AS Subcategory ,COUNT(*) AS TotalNumberProduct 
+SELECT ps.[Name] AS Subcategory, COUNT(*) AS TotalNumberProduct 
 FROM Production.Product p
 FULL JOIN Production.ProductSubcategory ps 
 ON ps.ProductSubcategoryID = p.ProductSubcategoryID
 GROUP BY ps.[Name];
 
 -- By Category
---WITH cte_subcategory (ProductSubcategoryID, [Name], TotalNumberProduct) AS (
---		SELECT ps.ProductSubcategoryID, ps.[Name] AS Subcategory ,COUNT(*) AS TotalNumberProduct 
---		FROM Production.Product p
---		FULL JOIN Production.ProductSubcategory ps 
---		ON ps.ProductSubcategoryID = p.ProductSubcategoryID
---		GROUP BY ps.ProductSubcategoryID, ps.[Name]
---)
-
---SELECT pc.[Name], COUNT(*) AS TotalNumberProduct 
---FROM Production.ProductSubcategory ps
---JOIN cte_subcategory cs 
---ON ps.ProductSubcategoryID = cs.ProductSubcategoryID
---JOIN Production.ProductCategory pc
---ON pc.ProductCategoryID = pc.ProductCategoryID
---GROUP BY pc.[Name];
-
---SELECT pc.ProductCategoryID, COUNT(*) 
---FROM Production.ProductSubcategory ps
---JOIN Production.ProductCategory pc
---ON ps.ProductCategoryID = pc.ProductCategoryID
---GROUP BY pc.ProductCategoryID;
+SELECT pc.[Name] AS Category, ps.[Name] AS Subcategory, COUNT(p.ProductID) AS TotalNumberProduct 
+FROM Production.ProductSubcategory ps
+FULL JOIN  Production.Product p
+ON ps.ProductSubcategoryID = p.ProductSubcategoryID
+FULL JOIN Production.ProductCategory pc
+ON pc.ProductCategoryID = ps.ProductCategoryID
+GROUP BY pc.[Name], ps.[Name]
+ORDER BY pc.[Name];
 
 --3. How many product do we have by productline? by style? by class?
+--by productline R = Road, M = Mountain, T = Touring, S = Standard
+SELECT ProductLine, COUNT(*) AS TotalNumberProduct 
+FROM Production.Product
+GROUP BY ProductLine;
 
+--by style W = Womens, M = Mens, U = Universal
+SELECT Style, COUNT(*) AS TotalNumberProduct 
+FROM Production.Product
+GROUP BY Style;
+
+--by class H = High, M = Medium, L = Low
+SELECT Class, COUNT(*) AS TotalNumberProduct 
+FROM Production.Product
+GROUP BY Class;
 
 --4. What are the product that aren't sell by the company anymore?
-
+SELECT *
+FROM Production.Product 
+WHERE SellEndDate IS NOT NULL;
 
 --5. How many product model do we have?
-
+SELECT COUNT(*) AS TotalNumberModel 
+FROM Production.ProductModel;
 
 --6. What is the average cost of product? by category? by sub-category? by productline? by style? by class?
+SELECT AVG(StandardCost) AS AverageProductCost
+FROM Production.Product
+WHERE StandardCost <> 0;
 
+-- By Subcategory
+SELECT ps.[Name] AS Subcategory , AVG(p.StandardCost) AS AverageProductCost
+FROM Production.Product p
+FULL JOIN Production.ProductSubcategory ps 
+ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+AND StandardCost <> 0
+GROUP BY ps.[Name];
+
+-- By Category
+SELECT pc.[Name] AS Category, ps.[Name] AS Subcategory, AVG(p.StandardCost) AS AverageProductCost 
+FROM Production.ProductSubcategory ps
+FULL JOIN  Production.Product p
+ON ps.ProductSubcategoryID = p.ProductSubcategoryID AND StandardCost <> 0
+FULL JOIN Production.ProductCategory pc
+ON pc.ProductCategoryID = ps.ProductCategoryID
+GROUP BY pc.[Name], ps.[Name]
+ORDER BY pc.[Name];
+
+-- By Productline
+SELECT ProductLine, AVG(StandardCost) AS AverageProductCost
+FROM Production.Product
+WHERE StandardCost <> 0
+GROUP BY ProductLine;
+
+-- By Style
+SELECT Style, AVG(StandardCost) AS AverageProductCost
+FROM Production.Product
+WHERE StandardCost <> 0
+GROUP BY Style;
+
+-- By Class
+SELECT Class, AVG(StandardCost) AS AverageProductCost
+FROM Production.Product
+WHERE StandardCost <> 0
+GROUP BY Class;
 
 --7. What is the average listing price of product? by category? by sub-category? by productline? by style? by class?
+SELECT AVG(ListPrice) AS AverageProductListingPrice
+FROM Production.Product
+WHERE ListPrice <> 0;
+
+-- By Subcategory
+SELECT ps.[Name] AS Subcategory , AVG(p.ListPrice) AS AverageProductListingPrice
+FROM Production.Product p
+FULL JOIN Production.ProductSubcategory ps 
+ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+AND p.ListPrice <> 0
+GROUP BY ps.[Name];
+
+-- By Category
+SELECT pc.[Name] AS Category, ps.[Name] AS Subcategory, AVG(p.ListPrice) AS AverageProductListingPrice 
+FROM Production.ProductSubcategory ps
+FULL JOIN  Production.Product p
+ON ps.ProductSubcategoryID = p.ProductSubcategoryID AND p.ListPrice <> 0
+FULL JOIN Production.ProductCategory pc
+ON pc.ProductCategoryID = ps.ProductCategoryID
+GROUP BY pc.[Name], ps.[Name]
+ORDER BY pc.[Name];
+
+-- By Productline
+SELECT ProductLine, AVG(ListPrice) AS AverageProductListingPrice
+FROM Production.Product
+WHERE ListPrice <> 0
+GROUP BY ProductLine;
+
+-- By Style
+SELECT Style, AVG(ListPrice) AS AverageProductListingPrice
+FROM Production.Product
+WHERE ListPrice <> 0
+GROUP BY Style;
+
+-- By Class
+SELECT Class, AVG(ListPrice) AS AverageProductListingPrice
+FROM Production.Product
+WHERE ListPrice <> 0
+GROUP BY Class;
+
+--8. How many product did we sell through our channels (physical stores, marketing, online stores, refferals) this year?
+SELECT *
+FROM Sales.SalesOrderHeader
+
+SELECT *
+FROM Sales.Store
 
 
---8. How many product did we sell through our channels (physical stores, marketing, online stores, refferals)?
-
+--Sales through marketing and referrals
+SELECT sr.[Name] AS Channels, COUNT(so.SalesOrderID) AS TotalNumberOrder
+FROM Sales.SalesOrderHeaderSalesReason so
+JOIN Sales.SalesReason sr 
+ON so.SalesReasonID = sr.SalesReasonID AND YEAR(so.ModifiedDate) = '2014' 
+AND (SR.ReasonType = 'Marketing' OR SR.ReasonType = 'Promotion' OR SR.[Name] = 'Review')
+GROUP BY sr.[Name];
 
 --9. How many purchase do we have by product during the year?
-
+SELECT od.ProductID, p.[Name] AS Product, COUNT(*) AS TotalNumberOrder
+FROM Sales.SalesOrderDetail od
+JOIN Sales.SalesOrderHeader oh
+ON od.SalesOrderID = oh.SalesOrderID 
+JOIN Production.Product p
+ON p.ProductID = od.ProductID
+WHERE YEAR(oh.OrderDate) = '2014'
+GROUP BY od.ProductID, p.[Name]
+ORDER BY TotalNumberOrder DESC;
 
 --10. How many purchase do we have by product during the year?
 
