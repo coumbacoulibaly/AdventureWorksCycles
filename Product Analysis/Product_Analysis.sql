b74a836111d9916226073039e9b31d0510d22b55
coumba <coumbacoulibaly09@gmail.com>
2023-01-17 13:43:15 +0000

    Update Product_Analysis.sql

--- Product Analysis/Product_Analysis.sql
+++ Product Analysis/Product_Analysis.sql
@@ -143,14 +143,17 @@ WHERE ListPrice <> 0
 GROUP BY Class;
 
 --8. How many product did we sell through our channels (physical stores, marketing, online stores, refferals) this year?
-SELECT *
+-- Online Store Orders 
+SELECT COUNT(*) AS TotalNumberOrder
 FROM Sales.SalesOrderHeader
+WHERE OnlineOrderFlag = '1' AND YEAR(OrderDate) = '2014';
 
-SELECT *
-FROM Sales.Store
+-- Physical Stores Orders 
+SELECT COUNT(*) AS TotalNumberOrder
+FROM Sales.SalesOrderHeader
+WHERE SalesPersonID IS NOT NULL AND YEAR(OrderDate) = '2014';
 
-
---Sales through marketing and referrals
+--Orders from marketing and referrals
 SELECT sr.[Name] AS Channels, COUNT(so.SalesOrderID) AS TotalNumberOrder
 FROM Sales.SalesOrderHeaderSalesReason so
 JOIN Sales.SalesReason sr 
@@ -169,10 +172,184 @@ WHERE YEAR(oh.OrderDate) = '2014'
 GROUP BY od.ProductID, p.[Name]
 ORDER BY TotalNumberOrder DESC;
 
---10. How many purchase do we have by product during the year?
+--10. Which product is the most purchase by customers during the past years?
+
+WITH cte_product_ordered ([YEAR], ProductID, ProductName, OrderNumber) AS (
+	SELECT YEAR(oh.OrderDate) as [YEAR], od.ProductID, p.[Name] AS ProductName, 
+			COUNT(*) as OrderNumber
+	FROM Sales.SalesOrderDetail od
+	JOIN Sales.SalesOrderHeader oh
+	ON od.SalesOrderID = oh.SalesOrderID 
+	JOIN Production.Product p
+	ON p.ProductID = od.ProductID
+	GROUP BY YEAR(oh.OrderDate), od.ProductID, p.[Name]
+)
+
+SELECT po.[YEAR], po.ProductID, po.ProductName, po.OrderNumber
+FROM cte_product_ordered po
+INNER JOIN 
+(SELECT [YEAR], MAX(OrderNumber) as MaxOrderNumber
+FROM cte_product_ordered 
+GROUP BY [YEAR]) pom
+ON po.[YEAR] = pom.[YEAR] AND po.OrderNumber = pom.MaxOrderNumber;
+
+--11. Which products are the most purchase by category? by sub-category?
+-- By Subcategory
+
+WITH cte_subcategory(ProductSubcategoryID, SubcategoryName, ProductID, ProductName) AS (
+	SELECT ps.ProductSubcategoryID, ps.[Name] AS SubcategoryName, p.ProductID, p.[Name] AS ProductName
+	FROM Production.Product p
+	FULL JOIN Production.ProductSubcategory ps 
+	ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+),
+cte_product_ordered (ProductSubcategoryID, SubcategoryName, ProductID, ProductName, OrderNumber) AS (
+	SELECT  s.ProductSubcategoryID,
+			s.SubcategoryName,
+			od.ProductID, 
+			s.ProductName, 
+			COUNT(*) as OrderNumber
+	FROM Sales.SalesOrderDetail od
+	JOIN Sales.SalesOrderHeader oh
+	ON od.SalesOrderID = oh.SalesOrderID 
+	FULL JOIN cte_subcategory s
+	ON s.ProductID = od.ProductID
+	WHERE YEAR(oh.OrderDate) = '2014'
+	GROUP BY s.ProductSubcategoryID, s.SubcategoryName, od.ProductID, s.ProductName
+)
+SELECT  po.ProductSubcategoryID, po.SubcategoryName, po.ProductID, po.ProductName, po.OrderNumber
+FROM cte_product_ordered po
+INNER JOIN 
+(SELECT ProductSubcategoryID, MAX(OrderNumber) as MaxOrderNumber
+FROM cte_product_ordered 
+GROUP BY ProductSubcategoryID) pom
+ON po.ProductSubcategoryID = pom.ProductSubcategoryID AND po.OrderNumber = pom.MaxOrderNumber
+ORDER BY po.OrderNumber DESC;
+
+-- By Category
+WITH cte_Category(Category, SubcategoryName, ProductID, ProductName) AS (
+	SELECT pc.[Name] AS Category, ps.[Name] AS SubcategoryName, p.ProductID, p.[Name] AS ProductName
+	FROM Production.ProductSubcategory ps
+	FULL JOIN  Production.Product p
+	ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+	FULL JOIN Production.ProductCategory pc
+	ON pc.ProductCategoryID = ps.ProductCategoryID
+	GROUP BY pc.[Name], ps.[Name], p.ProductID, p.[Name]
+),
+cte_product_ordered (Category, SubcategoryName, ProductID, ProductName, OrderNumber) AS (
+	SELECT  s.Category,
+			s.SubcategoryName,
+			od.ProductID, 
+			s.ProductName, 
+			COUNT(*) as OrderNumber
+	FROM Sales.SalesOrderDetail od
+	JOIN Sales.SalesOrderHeader oh
+	ON od.SalesOrderID = oh.SalesOrderID 
+	FULL JOIN cte_subcategory s
+	ON s.ProductID = od.ProductID
+	WHERE YEAR(oh.OrderDate) = '2014'
+	GROUP BY s.Category, s.SubcategoryName, od.ProductID, s.ProductName
+)
+SELECT  po.Category, po.SubcategoryName, po.ProductID, po.ProductName, po.OrderNumber
+FROM cte_product_ordered po
+INNER JOIN 
+(SELECT Category, MAX(OrderNumber) as MaxOrderNumber
+FROM cte_product_ordered 
+GROUP BY Category) pom
+ON po.Category = pom.Category AND po.OrderNumber = pom.MaxOrderNumber
+ORDER BY po.OrderNumber DESC;
+
+--12. What is the total revenue by each product this year?
+WITH cte_numberproduct (ProductID, Product, TotalNumberOrder) AS (
+	SELECT od.ProductID, p.[Name] AS Product, COUNT(*) AS TotalNumberOrder--, (  COUNT(*) * p.ListPrice ) AS TotalRevenue
+	FROM Sales.SalesOrderDetail od
+	JOIN Sales.SalesOrderHeader oh
+	ON od.SalesOrderID = oh.SalesOrderID 
+	JOIN Production.Product p
+	ON p.ProductID = od.ProductID
+	WHERE YEAR(oh.OrderDate) = '2014'
+	GROUP BY od.ProductID, p.[Name]
+	
+)
+SELECT p.ProductID, np.Product, np.TotalNumberOrder ,(np.TotalNumberOrder * p.ListPrice) AS TotalRevenue
+FROM Production.Product p
+JOIN cte_numberproduct np
+ON p.ProductID = np.ProductID
+ORDER BY TotalRevenue DESC;
+
+--13. Which product makes the highest revenue this year by category? by sub-category?
+-- By Subcategory
+WITH cte_subcategory(ProductSubcategoryID, SubcategoryName, ProductID, ProductName) AS (
+	SELECT ps.ProductSubcategoryID, ps.[Name] AS SubcategoryName, p.ProductID, p.[Name] AS ProductName
+	FROM Production.Product p
+	FULL JOIN Production.ProductSubcategory ps 
+	ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+),
+cte_numberproduct (ProductSubcategoryID, SubcategoryName, ProductID, ProductName, TotalNumberOrder) AS (
+	SELECT s.ProductSubcategoryID, s.SubcategoryName, od.ProductID, s.ProductName, COUNT(*) AS TotalNumberOrder
+	FROM Sales.SalesOrderDetail od
+	JOIN Sales.SalesOrderHeader oh
+	ON od.SalesOrderID = oh.SalesOrderID 
+	JOIN cte_subcategory s
+	ON s.ProductID = od.ProductID
+	WHERE YEAR(oh.OrderDate) = '2014'
+	GROUP BY s.ProductSubcategoryID, s.SubcategoryName, od.ProductID, s.ProductName
+),
+cte_revenue (ProductSubcategoryID, SubcategoryName, ProductID, ProductName, TotalNumberOrder, TotalRevenue) AS (
+	SELECT np.ProductSubcategoryID, np.SubcategoryName, np.ProductID, np.ProductName, 
+			TotalNumberOrder, 
+			(np.TotalNumberOrder * p.ListPrice) AS TotalRevenue
+	FROM Production.Product p
+	JOIN cte_numberproduct np	
+	ON p.ProductID = np.ProductID
+)
+SELECT  r.ProductSubcategoryID, r.SubcategoryName, r.ProductID, r.ProductName, r.TotalNumberOrder, r.TotalRevenue
+FROM cte_revenue r
+INNER JOIN (
+SELECT ProductSubcategoryID, MAX(TotalRevenue) AS MaxTotalRevenue
+FROM cte_revenue 
+GROUP BY ProductSubcategoryID) rm
+ON r.ProductSubcategoryID = rm.ProductSubcategoryID AND r.TotalRevenue = rm.MaxTotalRevenue
+ORDER BY r.TotalRevenue DESC;
 
 
---11. Which products are the most purchase by customers? (Same question for category, sub-category, productline, style and class)
 
-
---12. What is the total revenue by each? by category? by sub-category? by productline? by style? by class?
+-- By Category
+WITH cte_category(Category, SubcategoryName, ProductID, ProductName) AS (
+	SELECT pc.[Name] AS Category, ps.[Name] AS SubcategoryName, p.ProductID, p.[Name] AS ProductName
+	FROM Production.ProductSubcategory ps
+	FULL JOIN  Production.Product p
+	ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+	FULL JOIN Production.ProductCategory pc
+	ON pc.ProductCategoryID = ps.ProductCategoryID
+	GROUP BY pc.[Name], ps.[Name], p.ProductID, p.[Name]
+),
+cte_product_ordered (Category, SubcategoryName, ProductID, ProductName, TotalNumberOrder) AS (
+	SELECT  c.Category,
+			c.SubcategoryName,
+			od.ProductID, 
+			c.ProductName, 
+			COUNT(*) as TotalNumberOrder
+	FROM Sales.SalesOrderDetail od
+	JOIN Sales.SalesOrderHeader oh
+	ON od.SalesOrderID = oh.SalesOrderID 
+	FULL JOIN cte_category c
+	ON c.ProductID = od.ProductID
+	WHERE YEAR(oh.OrderDate) = '2014'
+	GROUP BY c.Category, c.SubcategoryName, od.ProductID, c.ProductName
+),
+cte_revenue (Category, SubcategoryName, ProductID, ProductName, TotalNumberOrder, TotalRevenue) AS (
+	SELECT np.Category, np.SubcategoryName, np.ProductID, np.ProductName, 
+			np.TotalNumberOrder, 
+			(np.TotalNumberOrder * p.ListPrice) AS TotalRevenue
+	FROM Production.Product p
+	JOIN cte_product_ordered np	
+	ON p.ProductID = np.ProductID
+)
+SELECT  r.Category, r.SubcategoryName, r.ProductID, r.ProductName, r.TotalNumberOrder, r.TotalRevenue
+FROM cte_revenue r
+INNER JOIN (
+SELECT Category, MAX(TotalRevenue) AS MaxTotalRevenue
+FROM cte_revenue 
+GROUP BY Category) rm
+ON r.Category = rm.Category AND r.TotalRevenue = rm.MaxTotalRevenue
+ORDER BY r.TotalRevenue DESC;
