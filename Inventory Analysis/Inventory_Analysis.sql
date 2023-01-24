efc4ebce98781d6a20bb5dae952967b0fc81393f
coumba <coumbacoulibaly09@gmail.com>
2023-01-24 12:44:07 +0000

    Update Inventory_Analysis.sql

--- Inventory Analysis/Inventory_Analysis.sql
+++ Inventory Analysis/Inventory_Analysis.sql
@@ -34,45 +34,21 @@ WITH cte_lead_time (WorkOrderID, TotalResourceHrs)	AS (
 SELECT AVG(TotalResourceHrs) AS AvgLeadTime
 FROM cte_lead_time;
 
---4. What is the days of supply (number of days that inventory will last at the current rate of consumption)?
 
-
-
-
-
---5. What is the Fill rate (percentage of customer orders that are fulfilled on time and in full)?
+--4. What is the Fill rate (percentage of customer orders that are fulfilled on time and in full)?
 SELECT COUNT(*) AS TotalFulfilledOrders, 
 		(COUNT(*) * 100.00/(SELECT COUNT(*) FROM Sales.SalesOrderHeader WHERE YEAR(OrderDate) = '2014')) AS FillRate
 FROM Sales.SalesOrderHeader
 WHERE DueDate >= ShipDate AND YEAR(OrderDate) = '2014';
 
---6. What is the Backorder rate (percentage of customer orders that cannot be fulfilled on time)?
+--5. What is the Backorder rate (percentage of customer orders that cannot be fulfilled on time)?
 SELECT COUNT(*) AS TotalUnfulfilledOrders, 
 		(COUNT(*) * 100.00/(SELECT COUNT(*) FROM Sales.SalesOrderHeader WHERE YEAR(OrderDate) = '2014')) AS BackOrderRate
 FROM Sales.SalesOrderHeader
 WHERE DueDate < ShipDate AND YEAR(OrderDate) = '2014'
 
----- Throught out the years
---SELECT YEAR(OrderDate) AS [Years],
---		COUNT(*) AS TotalUnfulfilledOrders, 
---		(COUNT(*) * 100.00/(SELECT COUNT(*) FROM Sales.SalesOrderHeader WHERE YEAR(OrderDate) = '2014')) AS BackOrderRate
---FROM Sales.SalesOrderHeader
---WHERE DueDate < ShipDate 
---GROUP BY YEAR(OrderDate);
-
---SELECT COUNT(*) 
---FROM Production.WorkOrderRouting 
---WHERE ScheduledEndDate < ActualEndDate AND YEAR(ActualStartDate) = '2014'
-
---SELECT *
---FROM Sales.SalesOrderHeader
---WHERE DueDate >= ShipDate AND YEAR(OrderDate) = '2014'
-
---7. What is the inventory turnover (number of times inventory is sold and replaced over a given period)?
-
-
---8. Calculate the Gross Margin (Net sales - cost of goods sold)
-SELECT ProductID, (ListPrice - StandardCost  ) AS GrossMargin
+--6. Calculate the Gross Margin 
+SELECT ProductID, (((ListPrice - StandardCost)/ListPrice) * 100) AS GrossMargin
 FROM Production.Product
 WHERE ListPrice <> 0 AND StandardCost <> 0;
 
