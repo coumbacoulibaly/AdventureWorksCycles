abc6d9fedd448b1613cbe77976df35ab67dc18c5
coumba <coumbacoulibaly09@gmail.com>
2023-01-24 12:23:48 +0000

    Update Inventory_Analysis.sql

--- Inventory Analysis/Inventory_Analysis.sql
+++ Inventory Analysis/Inventory_Analysis.sql
@@ -26,42 +26,16 @@ FROM Production.Location
 
 --3. What is the order lead time (time it takes to receive a product after placing an order)? 
 WITH cte_lead_time (WorkOrderID, TotalResourceHrs)	AS (
-SELECT WorkOrderID, SUM(ActualResourceHrs) AS TotalResourceHrs
-FROM Production.WorkOrderRouting
-WHERE YEAR(ModifiedDate) = '2014'
-GROUP BY WorkOrderID
+		SELECT WorkOrderID, SUM(ActualResourceHrs) AS TotalResourceHrs
+		FROM Production.WorkOrderRouting
+		WHERE YEAR(ModifiedDate) = '2014'
+		GROUP BY WorkOrderID
 )
 SELECT AVG(TotalResourceHrs) AS AvgLeadTime
 FROM cte_lead_time;
 
 --4. What is the days of supply (number of days that inventory will last at the current rate of consumption)?
-SELECT pin.ProductID, od.ProductID, 
-		SUM(od.OrderQty) AS QtySold, --Quantity of the product that has been sold since January 2014 to June 2014
-		SUM(pin.Quantity) AS StockLevel -- Current Stock level at August 2014
-		--((6 * SUM(pin.Quantity))/ SUM(od.OrderQty)) AS MonthsOfSupply
-FROM Production.ProductInventory pin
-FULL JOIN Sales.SalesOrderDetail od
-ON od.ProductID = pin.ProductID
-WHERE YEAR(pin.ModifiedDate) = 2014 AND YEAR(od.ModifiedDate) = 2014
-GROUP BY pin.ProductID, od.ProductID;
 
-SELECT ProductID, SUM(OrderQty) AS QtySold
-FROM Sales.SalesOrderDetail
-WHERE YEAR(ModifiedDate) = 2014
-GROUP BY ProductID
-
-SELECT ProductID, SUM(Quantity) AS StockLevel
-FROM Production.ProductInventory
-WHERE YEAR(ModifiedDate) = 2014
-GROUP BY ProductID;
-
-SELECT *
-FROM Production.ProductInventory
-WHERE YEAR(ModifiedDate) = 2014;
-
-SELECT * 
-FROM Sales.SalesOrderDetail
-WHERE YEAR(ModifiedDate) = 2014
 
 
 
